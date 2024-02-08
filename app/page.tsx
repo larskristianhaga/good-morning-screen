98296f1ae0fb9b727d86af3a11930e2428cb7215
Lars Kristian Haga <larskhaga@gmail.com>
2024-02-08 21:48:15 +0100

    Added Norwegian translation

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,20 @@ export default function Home() {
     };
   }, []);
 
+  const days = ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'];
+  const dayName = days[time.getDay()];
+
+  const dateOptions = { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit' };
+  const dateString = time.toLocaleDateString('no-NO', dateOptions);
+
   return (
     <main className="flex justify-center space-x-1.5">
       <iframe src="https://mon.ruter.no/departures/59.9181008472934-10.742226849607649/N4Igrgzgpgwg9gGzAWwHYBkCGBPOYAuIAXAGaYLQA0IARnJgE4AmExA2qBPnAA4AKCTAGMoASRbsQAOQDKAJSIzu-QSKIA2AAwBmAEy6Q1WQqW8BwqBt0AObSAC61JlEHYoTJY3wAVAJbIoYl1qAAtfJmcMX1QoViJQY0VlczUtPQMiNml5IgBFMBwiAHYAVgAWTQBaAEZrSoARX2hUQ2yFfMLSipq60wY4SoBxBigSZoACLgA6VsSO7GLyquqiyplo5tmc+cXulbWNqFQhkbGjyfwZo22CherNG2066uqpSoBZTAArOAYuMCOW3atyI90eAE4aq9KgBpL4uCAAU9Y12BnSWNUh6C+mBoDDAQLyINK2kh1Uhnx+f3wAJaqKJhTBtjJ4LeACkoAxUDRMDFuAwAOZQQj0nZM0mYt4AUYgeMR+EBopB4uerNhDGiAogX0RhLFD2ZNXZnO5vOFvyFIraDLuBqeRsqAFUEAgTcoeIrrfqIZVdG8AKIuzUQfFMZGeubKu11P2VAAbXE5yPG3gc9NMKgsVlskkjjOqJQl2lWAAkESRsAwHABfaioFA0TkAeRI9SgPC8YBGcWs1HwvnwruIIBgmHwIUmAGtyOMeIjxj8aDRWkwmrjXUxvAxeRAeL98O84M44mxHCAjuv3MR8PioKFwlAt8JJ-AkGhiGQKFBa5xkqoxBImRns4rjuJ4DA+P4gREJo94REc6DRLExDALWID1sgjYMC2bYdhBXbIUQvYgP2g7QSA3gEk4a40BuT6oLu+6Hse7BnhetFXqQ5BUCAYTOE+QgvogKAtFxX7VmeABunIQL4cCibo1ZAA" />
       <iframe src="https://www.yr.no/nb/innhold/1-72837/card.html?mode=dark"/>
       <div className="w-1/5 flex flex-col justify-around">
         <div className="text-4xl h-3/6">
-          <p>{time.toDateString()}</p>
-        </div>
-        <div className="text-4xl h-3/6">
-          <p>{time.toTimeString()}</p>
+          <p>{dateString}</p>
+          <p>{dayName}</p>
         </div>
       </div>
     </main>
