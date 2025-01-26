b45ee912cff2a343508b02a92c892661acf731d1
Lars Kristian Haga <lars.kristian.haga@cappelendamm.no>
2025-01-26 21:39:57 +0100

    Added noindex to html

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+            <meta content="none" name="robots"/>
+        </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
